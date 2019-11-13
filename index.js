f4a9309c6cd369b391622e8caa884797b8b1f743
Arnold Diep <tungd@uw.edu>
2019-11-12 18:20:34 -0800

    promting username, axios setup

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const questions = [
-  
-];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const html = require("./generateHTML.js");
 
-function writeToFile(fileName, data) {
- 
-}
+inquirer.prompt({
+    type: "input",
+    name: "username",
+    message: "What is your username?"
+}).then(function({ username }) {
+    const queryURL = `https://api.github.com/users/${username}`;
 
-function init() {
+    axios.get(queryURL).then(function(response) {
 
-}
-init();
+    })
+})
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+
+// }
+
+// function init() {
+
+// }
+// init();
