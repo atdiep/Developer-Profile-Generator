e6225a2082237b63926f66c13a1d4cae09b14927
Arnold Diep <tungd@uw.edu>
2019-11-13 18:38:12 -0800

    adding references and color choices

--- index.js
+++ index.js
@@ -3,20 +3,43 @@ const fs = require("fs");
 const axios = require("axios");
 const html = require("./generateHTML.js");
 
-inquirer.prompt({
-    type: "input",
-    name: "username",
-    message: "What is your username?"
-}).then(function({ username }) {
-    const queryURL = `https://api.github.com/users/${username}`;
+inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "What is your username?"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "Pick your prefered color showing on your profile.",
+        choices: [
+            "Blue",
+            "Green",
+            "Pink",
+            "Red"
+        ]
+    }]).then(function ({ username , color}) {
+        const queryURL = `https://api.github.com/users/${username}`;
+        //const starredURL = `https://api.github.com/users/${username}/starred`
 
-    axios.get(queryURL).then(function(response) {
-
+        axios.get(queryURL).then(function (response) {
+            console.log(response)
+            const user = response.data
+            const profileImg = user.avatar_url;
+            const username = user.login;
+            const location = user.location;
+            const github = user.html_url;
+            const blog = user.blog;
+            const bio = user.bio;
+            const publicRepos = user.public_repos;
+            const followers = user.followers;
+            // axios.get(starredURL).then(function(response) {
+            //     console.log(response)
+            // })
+            const following = user.following;
+        })
     })
-})
-// const questions = [
-
-// ];
 
 // function writeToFile(fileName, data) {
 
